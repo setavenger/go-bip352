d3b2d21cc33435ff399ab7637192bd376fccfad0
setavenger <github@snblago.com>
2025-04-06 15:32:59 +0200

    added some extra tooling

--- go.mod
+++ go.mod
@@ -3,17 +3,18 @@ module github.com/setavenger/go-bip352
 go 1.24.1
 
 require (
+	github.com/btcsuite/btcd v0.23.5-0.20231215221805-96c9fd8078fd
 	github.com/btcsuite/btcd/btcec/v2 v2.3.4
 	github.com/btcsuite/btcd/btcutil v1.1.5
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0
 	github.com/setavenger/blindbit-lib v0.0.0-20250601142252-0c5dd9a697be
 	github.com/setavenger/go-libsecp256k1 v0.0.0-20250601142217-61f26e074fd5
 	github.com/stretchr/testify v1.8.0
+	github.com/tyler-smith/go-bip39 v1.1.0
 	golang.org/x/crypto v0.17.0
 )
 
 require (
-	github.com/btcsuite/btcd v0.23.5-0.20231215221805-96c9fd8078fd // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
---
+++ keys.go
@@ -0,0 +1,109 @@
+package bip352
+
+import (
+	"github.com/btcsuite/btcd/btcutil/hdkeychain"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/tyler-smith/go-bip39"
+)
+
+// KeysFromMnemonic computes the scan and spend secret keys based on a mnemonic, a seedphrase (optional, leave as empty string if not needed) and chain
+func KeysFromMnemonic(
+	mnemonic, seedPassphrase string,
+	mainnet bool,
+) (
+	scanSecret, spendSecret [32]byte,
+	err error,
+) {
+	// Generate a Bip32 HD wallet for the mnemonic and a user supplied password
+	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, seedPassphrase)
+	if err != nil {
+		return
+	}
+
+	chainParams := chaincfg.MainNetParams
+	if !mainnet {
+		chainParams = chaincfg.SigNetParams
+	}
+
+	master, err := hdkeychain.NewMaster(seed, &chainParams)
+	if err != nil {
+		return
+	}
+
+	return DeriveKeysFromMaster(master, mainnet)
+}
+
+func DeriveKeysFromMaster(
+	master *hdkeychain.ExtendedKey,
+	mainnet bool,
+) (
+	scanSecret, spendSecret [32]byte,
+	err error,
+) {
+	/*
+		ScanDerivationPath = "m/352'/0'/0'/1'/0";
+		SpendDerivationPath = "m/352'/0'/0'/0'/0";
+	*/
+
+	// m/352'
+	purpose, err := master.Derive(352 + hdkeychain.HardenedKeyStart)
+	if err != nil {
+		return
+	}
+
+	var coinType *hdkeychain.ExtendedKey
+
+	if mainnet {
+		// m/352'/0'
+		coinType, err = purpose.Derive(0 + hdkeychain.HardenedKeyStart)
+		if err != nil {
+			return
+		}
+	} else {
+		// m/352'/1'
+		coinType, err = purpose.Derive(1 + hdkeychain.HardenedKeyStart)
+		if err != nil {
+			return
+		}
+	}
+
+	// m/352'/0'/0'
+	acct0, err := coinType.Derive(0 + hdkeychain.HardenedKeyStart)
+	if err != nil {
+		return
+	}
+
+	// m/352'/0'/0'/1'
+	scanExternal, err := acct0.Derive(1 + hdkeychain.HardenedKeyStart)
+	if err != nil {
+		return
+	}
+
+	// m/352'/0'/0'/0'
+	spendExternal, err := acct0.Derive(0 + hdkeychain.HardenedKeyStart)
+	if err != nil {
+		return
+	}
+
+	scanKey, err := scanExternal.Derive(0)
+	if err != nil {
+		return
+	}
+
+	spendKey, err := spendExternal.Derive(0)
+	if err != nil {
+		return
+	}
+
+	secretKeyScan, err := scanKey.ECPrivKey()
+	if err != nil {
+		return
+	}
+
+	secretKeySpend, err := spendKey.ECPrivKey()
+	if err != nil {
+		return
+	}
+
+	return ConvertToFixedLength32(secretKeyScan.Serialize()), ConvertToFixedLength32(secretKeySpend.Serialize()), nil
+}
